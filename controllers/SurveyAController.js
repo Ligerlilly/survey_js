3e333767aa03883f1a580e4ffb1b81c0abff3279
Jason <quizathaderat@yahoo.com>
2015-08-31 16:49:01 -0700

    Add ng-app to html tag

--- controllers/SurveyAController.js
+++
@@ -1,6 +0,0 @@
-surveyJS.controller('SurveyACtrl', function SurveyACtrl($sope, SurveyFactory) {
-  $scope.questionA = "Whats your favorite color?";
-  console.log($scope.questionA);
-  $scope.answers = SurveyFactory.answers;
-  $scope.SurveyFactory = SurveyFactory;
-});
---
+++ js/SurveyAController.js
@@ -0,0 +1,5 @@
+surveyJS.controller('SurveyACtrl', ['$scope', function SurveyACtrl($scope) {
+  $scope.questionA = "Whats your favorite color?";
+  // $scope.answers = SurveyFactory.answers;
+  // $scope.SurveyFactory = SurveyFactory;
+}]);
